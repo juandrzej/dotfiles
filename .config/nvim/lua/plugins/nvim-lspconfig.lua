506ede34db48ff15c1ac068791269d9d90e24ec4
juandrzej <programming.yippee729@passinbox.com>
2025-08-01 20:38:07 +0200

    clean all lint code

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -247,12 +247,23 @@ return {
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'stylua', -- Used to format Lua code
+      -- linters
+      'vale',
+      'markdownlint',
+      'jsonlint',
+      'yamllint',
+      'hadolint',
+      'shellcheck',
+      'selene',
+      'ruff',
+      'mypy',
+      'stylelint',
+      'htmlhint',
+      -- formatters
+      'stylua',
       'lua_ls',
       'bashls',
       'pyright',
-      'ruff',
-      'mypy',
     })
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
