5e8d132a1dd9c674f454072d17442f0972074ad4
juandrzej <programming.yippee729@passinbox.com>
2025-09-21 18:57:36 +0200

    add fedora and arch setup to test from remote

--- setup/setup.sh
+++ setup/setup.sh
@@ -6,7 +6,13 @@ debian)
 	sudo apt update && sudo apt upgrade -y
 	sudo apt install -y git ansible
 	;;
-fedora) echo "fedora" ;;
-arch) echo "arch" ;;
+fedora)
+	sudo dnf update
+	sudo dnf install -y git ansible
+	;;
+arch)
+	sudo pacman -Syu --noconfirm
+	sudo pacman -S --noconfirm git ansible
+	;;
 *) echo "This distribution is not set up" ;;
 esac
