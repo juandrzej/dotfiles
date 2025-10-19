eb20b5bda2ee4c3990ba0018e13e3edfabdf53bf
juandrzej <programming.yippee729@passinbox.com>
2025-10-19 23:59:53 +0200

    add swaybg, remove some icons

--- .config/waybar/network.sh
+++ .config/waybar/network.sh
@@ -4,25 +4,25 @@ INTERFACE=$(ip route | grep default | awk '{print $5}' | head -n1)
 
 # Check if we have any interface at all
 if [ -z "$INTERFACE" ]; then
-    echo "󰈂 Disconnected"
-    exit 0
+	echo "󰈂 Disconnected"
+	exit 0
 fi
 
 # Check if the interface is actually up and has an IP
 IP=$(ip addr show $INTERFACE 2>/dev/null | grep -o 'inet [0-9.]*' | cut -d' ' -f2)
 
 if [ -z "$IP" ]; then
-    echo "󰈂 Disconnected"
-    exit 0
+	echo "󰈂 Disconnected"
+	exit 0
 fi
 
 # Check interface type and show appropriate icon/text
 if [[ $INTERFACE == *"pvpn"* ]]; then
-    echo "🔒VPN"
+	echo "VPN"
 elif [[ $INTERFACE == "eth"* ]] || [[ $INTERFACE == "enp"* ]]; then
-    echo "󰈀 Ethernet"
+	echo "󰈀 Ethernet"
 elif [[ $INTERFACE == "wlan"* ]] || [[ $INTERFACE == "wlp"* ]]; then
-    echo "󰖩 WiFi"
+	echo "󰖩 WiFi"
 else
-    echo "󰈀 $INTERFACE"
+	echo "󰈀 $INTERFACE"
 fi
