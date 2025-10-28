ad4429354cf3dff062e3858a11d3649c1b810fbb
juandrzej <programming.yippee729@passinbox.com>
2025-10-28 22:00:58 +0100

    move arch install steps during chroot to separate file

---
+++ setup/arch_chroot_setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+set -e # exit on any error
+
+ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+cat >/etc/vconsole.conf <<EOF
+KEYMAP=us
+FONT=ter-v16b
+EOF
+echo "juandrzej-arch" >/etc/hostname
+
+# adding pw for root and user
+passwd
+useradd -m -G wheel,users juandrzej
+passwd juandrzej
+# enable sudo for wheel group
+echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >/etc/sudoers.d/wheel
+chmod 440 /etc/sudoers.d/wheel
+systemctl enable NetworkManager
+
+# bootloader
+bootctl install
+
+# create boot entry
+cat >/boot/loader/entries/arch.conf <<EOF
+title   Arch Linux
+linux   /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux.img
+options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rootflags=subvol=@ rw
+EOF
+
+# create fallback boot entry
+cat >/boot/loader/entries/arch-fallback.conf <<EOF
+title   Arch Linux (fallback)
+linux   /vmlinuz-linux
+initrd  /amd-ucode.img
+initrd  /initramfs-linux-fallback.img
+options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rootflags=subvol=@ rw
+EOF
+
+# loader configuration
+cat >/boot/loader/loader.conf <<EOF
+default arch.conf
+timeout 2
+console-mode max
+editor  no
+EOF
+
+exit
--- setup/arch_setup.sh
+++ setup/arch_setup.sh
@@ -37,60 +37,8 @@ pacstrap -K /mnt base linux linux-firmware amd-ucode base-devel vi vim networkma
 # configure filesystem
 genfstab -U /mnt >>/mnt/etc/fstab
 
-# all below must go to separate script???
 arch-chroot /mnt
 
-ln -sf /usr/share/zoneinfo/Europe/Warsaw /etc/localtime
-hwclock --systohc
-
-echo "en_US.UTF-8 UTF-8" >/etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" >/etc/locale.conf
-cat >/etc/vconsole.conf <<EOF
-KEYMAP=us
-FONT=ter-v16b
-EOF
-echo "juandrzej-arch" >/etc/hostname
-
-# adding pw for root and user
-passwd
-useradd -m -G wheel,users juandrzej
-passwd juandrzej
-# enable sudo for wheel group
-echo "%wheel ALL=(ALL:ALL) NOPASSWD: ALL" >/etc/sudoers.d/wheel
-chmod 440 /etc/sudoers.d/wheel
-systemctl enable NetworkManager
-
-# bootloader
-bootctl install
-
-# create boot entry
-cat >/boot/loader/entries/arch.conf <<EOF
-title   Arch Linux
-linux   /vmlinuz-linux
-initrd  /amd-ucode.img
-initrd  /initramfs-linux.img
-options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rootflags=subvol=@ rw
-EOF
-
-# create fallback boot entry
-cat >/boot/loader/entries/arch-fallback.conf <<EOF
-title   Arch Linux (fallback)
-linux   /vmlinuz-linux
-initrd  /amd-ucode.img
-initrd  /initramfs-linux-fallback.img
-options root=UUID=$(blkid -s UUID -o value /dev/nvme0n1p3) rootflags=subvol=@ rw
-EOF
-
-# loader configuration
-cat >/boot/loader/loader.conf <<EOF
-default arch.conf
-timeout 2
-console-mode max
-editor  no
-EOF
-
 # finish
-exit
 umount -R /mnt
 reboot
