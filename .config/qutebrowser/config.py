a182d82c6aa2e96d4506b4968531c640abf0f1ff
juandrzej <programming.yippee729@passinbox.com>
2025-11-01 18:19:44 +0100

    add bookmarks, quickmarks, adjust config for yt rym and privacy settings

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,17 +5,25 @@ c.statusbar.position = "bottom"
 c.statusbar.show = "always"
 c.url.default_page = "https://qwant.com"
 c.url.start_pages = ["https://qwant.com"]
-
-# Search engines
 c.url.searchengines = {
     "DEFAULT": "https://qwant.com/?q={}",
     "aw": "https://wiki.archlinux.org/?search={}",
-    "y": "https://www.youtube.com/results?search_query={}"
+    "yt": "https://www.youtube.com/results?search_query={}",
+    "rym": "https://rateyourmusic.com/search?searchterm={}",
 }
 
-# Keybindings - reverse J and K for tab switching
+### Keybindings 
+# reverse J and K for tab switching
 config.bind('J', 'tab-prev')
 config.bind('K', 'tab-next')
 
-# Theme
+# Privacy settings
+c.content.headers.accept_language = "en-US,en;q=0.5"
+c.content.headers.custom = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
+# these may break workflow
+# c.content.canvas_reading = False
+# c.content.webgl = False
+# c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+
+# source theme from designated file
 config.source('tokyonight_theme.py')
