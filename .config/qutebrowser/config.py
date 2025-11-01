ec84504524720c938cc67359ee299550d7348ed9
juandrzej <programming.yippee729@passinbox.com>
2025-11-01 22:32:43 +0100

    add qute config reload and edit shortcuts + nvim default; adjust alacritty to new swayfx; add swayfx additions; add two new AI quickmarks

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,4 +1,5 @@
 # General settings
+c.editor.command = ["alacritty", "-e", "nvim", "{file}"]
 config.load_autoconfig(False)
 c.auto_save.session = True
 c.statusbar.position = "bottom"
@@ -19,14 +20,16 @@ c.url.searchengines = {
 # reverse J and K for tab switching
 config.bind('J', 'tab-prev')
 config.bind('K', 'tab-next')
+config.bind('<Alt-r>', 'config-source')
+config.bind('<Alt-e>', 'config-edit')
 
 # Privacy settings
 c.content.headers.accept_language = "en-US,en;q=0.5"
 c.content.headers.custom = {"accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"}
 # these may break workflow
-# c.content.canvas_reading = False
-# c.content.webgl = False
-# c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; rv:68.0) Gecko/20100101 Firefox/68.0"
+c.content.canvas_reading = False
+c.content.webgl = False
+c.content.headers.user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
 
 # source theme from designated file
 config.source('tokyonight_theme.py')
